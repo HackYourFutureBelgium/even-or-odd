6be88fa4132c6c4b585964d209b01b5d5ec67786
Abdulrazzak <abdulrssak@gmail.com>
2020-03-26 21:10:36 +0100

    handlers

--- handlers/display-nany.js
+++ handlers/display-nany.js
@@ -1,6 +1,13 @@
 function nanyHandler() {
   debugger;
   // write me!
+strings.displayed = "NaNy";
+const currentDisplayed = strings.displayed;
+const currentArray = strings[currentDisplayed];
+const currentListComponent = renderList(urrentArray, currentDisplayed);
+const listContainer = document.getElementById('selected');
+
+listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
--- handlers/display-odds.js
+++ handlers/display-odds.js
@@ -2,6 +2,14 @@ function oddsHandler() {
   debugger;
   // write me!
 
+  strings.displayed = "odds";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+  const currentListComponent = renderList(urrentArray, currentDisplayed);
+  const listContainer = document.getElementById('selected');
+  
+  listContainer.appendChild(currentListComponent);
+
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'odds',
